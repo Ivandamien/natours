ce420299527a4b9b6f4f046139451975ed323018
Ivan Okello <ivandamieno99@gmail.com>
2022-08-25 18:24:25 +0300

    Improved stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -66,19 +66,46 @@ exports.getCheckoutSession = catchAsync(async(req, res, next) => {
 })
 
 
-exports.createBookingCheckout = catchAsync(async(req, res, next) => {
-    const { tour, user, price } = req.query;
+// exports.createBookingCheckout = catchAsync(async(req, res, next) => {
+//     const { tour, user, price } = req.query;
 
-    if (!tour && !price && !user) return next();
+//     if (!tour && !price && !user) return next();
 
+//     await Booking.create({ tour, user, price });
+
+//     res.redirect(originalUrl.split('?')[0])
+//         // next();
+// })
+
+const createBookingCheckout = async session => {
+    const tour = session.client_reference_id;
+
+    const user = (await User.findOne({ email: session.customer_email })).id;
+    const price = session.line_items[0].price_data.unit_amount / 100
     await Booking.create({ tour, user, price });
 
-    res.redirect(originalUrl.split('?')[0])
-        // next();
-})
+}
 
 
+exports.webhookCheckout = (req, res, next) => {
+    const signature = req.headers['stripe-signature'];
+    let event
+    try {
+        event = stripe.webhooks.constructEvent(
+            req.body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET)
 
+    } catch (err) {
+        return res.status(400).send(`webhook error: ${err.message}`)
+    }
+    if (event.type === `checkout.session.complete`)
+        createBookingCheckout(event.data.object);
+
+    res.status(200).json({ received: true })
+
+
+}
 
 exports.createBooking = factory.createOne(Booking)
 exports.getBooking = factory.getOne(Booking)
